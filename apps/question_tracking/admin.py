6f880c022851868fdf01986a85911997b40c784f
anilcelik0 <anilcelik046@gmail.com>
2024-01-09 23:55:15 +0300

    added questions models

--- apps/question_tracking/admin.py
+++ apps/question_tracking/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import QuestionMainTopic, QuestionRecord, QuestionSubTopic
 
 # Register your models here.
+
+
+class QuestionMainTopicAdmin(admin.ModelAdmin):
+    readonly_fields = ["color"]
+    
+
+admin.site.register(QuestionMainTopic, QuestionMainTopicAdmin)
+admin.site.register(QuestionRecord)
+admin.site.register(QuestionSubTopic)
--- apps/question_tracking/models.py
+++ apps/question_tracking/models.py
@@ -1,13 +1,75 @@
 from collections.abc import Iterable
 from django.db import models
-from apps.models import BaseModel, SubTopic
+from apps.models import BaseModel, MainTopic, SubTopic
+from django.contrib.auth import get_user_model
+from random import randint
 
 # Create your models here.
 
+def random_rgb():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    rand_color_string = 'rgb('+str(r)+','+str(g)+','+str(b)+')'
+    return rand_color_string
 
-class Questions(BaseModel):
-    sub_topic = models.ForeignKey(SubTopic, on_delete=models.CASCADE, related_name="sub_topic")
-    questions_count = models.IntegerField()
-    correct_answer = models.IntegerField()
-    wrong_answer = models.IntegerField()
+class QuestionMainTopic(BaseModel):    
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="question")
+    main_topic = models.ForeignKey(MainTopic, on_delete=models.CASCADE, related_name="question_main_topic")
+    color = models.CharField(max_length=20, null=True, blank=True)
+    
+    target = models.IntegerField(null=True, blank=True)
+    target_is_complated = models.BooleanField(default=False)
+    
+    @property
+    def complated(self):
+        count = 0
+        try:
+            for records in self.question_main.all():
+                for record in records.topic.all():
+                    count += record.question_count
+            
+            return count
+        
+        except:
+            return count
+    
+    @property
+    def total(self):
+        if self.target:
+            return self.target
+        else:
+            return 0
+    
+    def save(self, *args, **kwargs):
+        self.color = random_rgb()
+        return super().save(*args, **kwargs)
+
+
+class QuestionSubTopic(BaseModel):
+    main_topic = models.ForeignKey(QuestionMainTopic, on_delete=models.CASCADE, related_name="question_main")
+    sub_topic = models.ForeignKey(SubTopic, null=True, blank=True, on_delete=models.CASCADE, related_name="question_sub")
+    color = models.CharField(max_length=20, null=True, blank=True)
+    @property
+    def complated(self):
+        count = 0
+        try:
+            for record in self.topic.all():
+                count += record.question_count
+            
+            return count
+        
+        except:
+            return count
+    
+    def save(self, *args, **kwargs):
+        self.color = random_rgb()
+        return super().save(*args, **kwargs)
+
+
+class QuestionRecord(BaseModel):
+    topic = models.ForeignKey(QuestionSubTopic, on_delete=models.CASCADE, related_name="topic")
+    question_count = models.IntegerField(null=True, blank=True)
+    correct_answer = models.IntegerField(null=True, blank=True)
+    wrong_answer = models.IntegerField(null=True, blank=True)
     
