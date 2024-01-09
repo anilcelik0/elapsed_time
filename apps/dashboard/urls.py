7825a9ffcf21b95675874dd51d6aee12fc941cab
anilcelik0 <anilcelik046@gmail.com>
2024-01-09 21:24:18 +0300

    base and config argon theme

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from .views import index, profile, sign_in, sign_up
 
 urlpatterns = [
+    path("", index, name="index"),
+    path("profile", profile, name="profile"),
+    path("sign-in", sign_in, name="sign_in"),
+    path("sign-up", sign_up, name="sign_up"),
 ]
