5bf51a53a02020ad72994689976e2c6a9935644a
Anıl Çelik <anilcelik046@gmail.com>
2024-01-09 21:51:45 +0300

    Update models.py

--- apps/models.py
+++ apps/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    crelated_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_modified_date = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     class Meta:
