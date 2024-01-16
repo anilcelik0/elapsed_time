d0bca87a6883cb0e0a545a25a5c0a016ec244f73
anilcelik0 <anilcelik046@gmail.com>
2024-01-17 01:55:51 +0300

    edit settings file

--- elapsed_time/wsgi.py
+++ elapsed_time/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elapsed_time.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elapsed_time.settings')
 
 application = get_wsgi_application()
