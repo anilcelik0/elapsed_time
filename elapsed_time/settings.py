40c46aecfcbe8ff583010a06d58c8a5aecc07c01
anilcelik0 <anilcelik046@gmail.com>
2024-02-05 17:33:46 +0300

    change static

--- elapsed_time/settings.py
+++ elapsed_time/settings.py
@@ -137,10 +137,12 @@ LOGOUT_REDIRECT_URL = "login"  # Route defined in home/urls.py
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
